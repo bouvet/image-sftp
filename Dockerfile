9260451d858992ee3a15febeb9a32bbc7a34e950
Trond Aabakken <trond.aabakken@bouvet.no>
2022-04-27 10:11:21 +0200

    Changed to slim bullseye and back to flask

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,7 @@
-FROM python:3-alpine
+FROM python:3.9-slim-bullseye
 COPY ./service /service
 
-RUN apk update
-RUN apk add openssl-dev libffi-dev musl-dev gcc make
-RUN apk add build-base jpeg-dev zlib-dev
-ENV LIBRARY_PATH=/lib:/usr/lib
 RUN pip install --upgrade pip
-
 RUN pip install -r /service/requirements.txt
 
 EXPOSE 5000/tcp
--- service/image_sftp.py
+++ service/image_sftp.py
@@ -70,17 +70,4 @@ def decode():
     )
 
 if __name__ == '__main__':
-    cherrypy.tree.graft(app, '/')
-
-    # Set the configuration of the web server to production mode
-    cherrypy.config.update({
-        'environment': 'production',
-        'engine.autoreload_on': False,
-        'log.screen': True,
-        'server.socket_port': 5000,
-        'server.socket_host': '0.0.0.0'
-    })
-
-    # Start the CherryPy WSGI web server
-    cherrypy.engine.start()
-    cherrypy.engine.block()
+    app.run(debug=True, host='0.0.0.0', threaded=True, port=os.environ.get('port',5000))
